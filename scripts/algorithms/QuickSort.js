144b4440b2c438030ea7c689805ad9defa989946
Cameron Kelly <cameron.kelly@utdallas.edu>
2024-05-17 19:36:44 -0500

    Refactored QuickSort for easier inheritence with alternate methods

--- scripts/algorithms/QuickSort.js
+++ scripts/algorithms/QuickSort.js
@@ -5,8 +5,29 @@ class QuickSort extends Sorter {
 
     * recurse(start, end) {
         if(start >= end) return;
+        
+        //chooses a pivot and moves it to the end
+        yield* this.choosePivot(start, end);
 
         //partition
+        yield* this.partition(start, end)
+    }
+
+    * choosePivot(start, end) {
+        //get partition as median of three and move to end
+        let mid = Math.floor((start+end)/2);
+        if(this.doCompare(start, mid)) this.doSwap(start, mid);
+        yield;
+        if(this.doCompare(mid, end)) this.doSwap(mid, end);
+        yield;
+        if(this.doCompare(start, mid)) this.doSwap(start, mid);
+        yield;
+
+        this.doSwap(mid, end);
+        yield;
+    }
+
+    * partition(start, end) {
         let i = start;
         for(let j = start; j < end; j++) {
             if(this.doCompare(end, j)) {
