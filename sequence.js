05309ead110d8d4662d9d002779f005492cc4390
Cameron Kelly <cameron.kelly@utdallas.edu>
2024-05-20 07:15:51 -0500

    Moved code from sketch.js to sequence.js file

---
+++ sequence.js
@@ -0,0 +1,48 @@
+// imgAddress = "https://upload.wikimedia.org/wikipedia/en/2/27/Bliss_%28Windows_XP%29.png";
+
+function loadSequence() {
+    flashing = true;
+    fullSound = false;
+
+    vis = new RainbowBarVisualizer(img, 1);
+
+    algIterator = algSeries([
+        //bubble sorts
+        {n: Math.pow(2, 9), speed: 10, alg: new Shuffle()},
+        {n: Math.pow(2, 9), speed: 100, alg: new BubbleSort()},
+        {n: Math.pow(2, 9), speed: 10, alg: new Shuffle()},
+        {n: Math.pow(2, 9), speed: 100, alg: new CocktailSort()},
+        {n: Math.pow(2, 9), speed: 10, alg: new Shuffle()},
+        {n: Math.pow(2, 9), speed: 100, alg: new OddEvenSort()},
+        {n: Math.pow(2, 11), speed: 40, alg: new Shuffle()},
+        {n: Math.pow(2, 11), speed: 100, alg: new CombSort({shrink: 1.3})},
+
+        //insertion sorts
+        {n: Math.pow(2, 10), speed: 20, alg: new Shuffle()},
+        {n: Math.pow(2, 10), speed: 100, alg: new InsertionSort()},
+        {n: Math.pow(2, 9), speed: 10, alg: new Shuffle()},
+        {n: Math.pow(2, 9), speed: 100, alg: new GnomeSort()},
+        {n: Math.pow(2, 12), speed: 80, alg: new Shuffle()},
+        {n: Math.pow(2, 12), speed: 100, alg: new ShellSort({shrink: 2.5})},
+
+        //selection sorts
+        {n: Math.pow(2, 9), speed: 10, alg: new Shuffle()},
+        {n: Math.pow(2, 9), speed: 100, alg: new SelectionSort()},
+        {n: Math.pow(2, 9), speed: 10, alg: new Shuffle()},
+        {n: Math.pow(2, 9), speed: 100, alg: new DoubleSelectionSort()},
+        {n: Math.pow(2, 12), speed: 80, alg: new Shuffle()},
+        {n: Math.pow(2, 12), speed: 100, alg: new HeapSort()},
+        
+        //quicksorts
+        {n: Math.pow(2, 12), speed: 80, alg: new Shuffle()},
+        {n: Math.pow(2, 12), speed: 100, alg: new QuickSort({partitionType:"LR"})},
+        {n: Math.pow(2, 12), speed: 80, alg: new Shuffle()},
+        {n: Math.pow(2, 12), speed: 100, alg: new QuickSort({partitionType:"LL"})},
+
+        //mergesorts
+        {n: Math.pow(2, 12), speed: 80, alg: new Shuffle()},
+        {n: Math.pow(2, 12), speed: 100, alg: new MergeSort()},
+        {n: Math.pow(2, 12), speed: 80, alg: new Shuffle()},
+        {n: Math.pow(2, 12), speed: 100, alg: new IterativeMergeSort()},
+    ]);
+}
--- sketch.js
+++ sketch.js
@@ -1,20 +1,17 @@
 p5.disableFriendlyErrors = true;
 
 let visCanvas;
-let vis;
-let arr;
-
-let waiting = false;
+let img;
 
 let speed = 0.1;
 let timer = 0;
+let waiting = false;
 
-let img;
-
-let imgAddress = "https://upload.wikimedia.org/wikipedia/en/2/27/Bliss_%28Windows_XP%29.png"
+let imgAddress;
 let flashing = false;
 let fullSound = false;
 let algIterator;
+let vis;
 
 function preload() {
     //load image, default to Lena if no address is specified
@@ -26,53 +23,9 @@ function setup() {
     visCanvas = createCanvas(1920, 1080);
     visCanvas.parent("visualizer-div");
 
-    arr = Array.from(Array(256).keys());
+    loadSequence();
 
-    vis = new ImageVisualizer(img, 1);
-    vis.useList(arr);
-    vis.useAlgorithm(new Shuffle());
-
-    algIterator = algSeries([
-        //bubble sorts
-        {n: Math.pow(2, 11), speed: 100, alg: new Shuffle()},
-        {n: Math.pow(2, 11), speed: 500, alg: new BubbleSort()},
-        {n: Math.pow(2, 11), speed: 100, alg: new Shuffle()},
-        {n: Math.pow(2, 11), speed: 500, alg: new CocktailSort()},
-        {n: Math.pow(2, 11), speed: 100, alg: new Shuffle()},
-        {n: Math.pow(2, 11), speed: 500, alg: new OddEvenSort()},
-        {n: Math.pow(2, 12), speed: 100, alg: new Shuffle()},
-        {n: Math.pow(2, 12), speed: 500, alg: new CombSort({shrink: 1.3})},
-
-        //insertion sorts
-        {n: Math.pow(2, 12), speed: 100, alg: new Shuffle()},
-        {n: Math.pow(2, 12), speed: 500, alg: new InsertionSort()},
-        {n: Math.pow(2, 11), speed: 50, alg: new Shuffle()},
-        {n: Math.pow(2, 11), speed: 1000, alg: new GnomeSort()},
-        {n: Math.pow(2, 17), speed: 1000, alg: new Shuffle()},
-        {n: Math.pow(2, 17), speed: 1000, alg: new ShellSort({shrink: 2.5})},
-
-        //selection sorts
-        {n: Math.pow(2, 11), speed: 100, alg: new Shuffle()},
-        {n: Math.pow(2, 11), speed: 1000, alg: new SelectionSort()},
-        {n: Math.pow(2, 11), speed: 100, alg: new Shuffle()},
-        {n: Math.pow(2, 11), speed: 1000, alg: new DoubleSelectionSort()},
-        {n: Math.pow(2, 17), speed: 1000, alg: new Shuffle()},
-        {n: Math.pow(2, 17), speed: 1000, alg: new HeapSort()},
-        
-        //quicksorts
-        {n: Math.pow(2, 17), speed: 1000, alg: new Shuffle()},
-        {n: Math.pow(2, 17), speed: 1000, alg: new QuickSort({partitionType:"LR"})},
-        {n: Math.pow(2, 17), speed: 1000, alg: new Shuffle()},
-        {n: Math.pow(2, 17), speed: 1000, alg: new QuickSort({partitionType:"LL"})},
-
-        //mergesorts
-        {n: Math.pow(2, 17), speed: 1000, alg: new Shuffle()},
-        {n: Math.pow(2, 17), speed: 1000, alg: new MergeSort()},
-        {n: Math.pow(2, 17), speed: 1000, alg: new Shuffle()},
-        {n: Math.pow(2, 17), speed: 1000, alg: new IterativeMergeSort()},
-    ]);
-
-    algIterator.next()
+    algIterator.next();
 }
 
 //p5js draw function
@@ -111,10 +64,9 @@ function mousePressed() {
 function* algSeries(list) {
     for(let i in list) {
         if(i == 0 || list[i].n != list[i-1].n) {
-            arr = Array.from(Array(list[i].n).keys());
-            vis.useList(arr);
+            vis.useList(Array.from(Array(list[i].n).keys()));
             vis.done = true;
-            yield;
+            if(i > 0) yield;
         }
         vis.useAlgorithm(list[i].alg);
         speed = list[i].speed;
