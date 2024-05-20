1e4c64ffcd15c0937dd50d183ae867f1fe846462
Cameron Kelly <cameron.kelly@utdallas.edu>
2024-05-20 05:45:47 -0500

    Added IterativeMergeSort

---
+++ scripts/algorithms/IterativeMergeSort.js
@@ -0,0 +1,46 @@
+class IterativeMergeSort extends Sorter {
+    constructor() {
+        super();
+
+        this.name = "Bottom-Up Merge Sort";
+    }
+    
+    * sort() {
+        for(let w = 1; w < this.arr.length; w *= 2) {
+            for(let i = 0; i < this.arr.length; i += w*2) {
+                yield* this.merge(i, Math.min(i+w, this.arr.length), Math.min(i+w*2, this.arr.length));
+            }
+        }
+    }
+
+    * merge(start, mid, end) {
+        let startpointer = start;
+        let midpointer = mid;
+        let mergeArray = [];
+        while(startpointer < mid && midpointer < end) {
+            if(this.doCompare(midpointer, startpointer)) {
+                mergeArray.push(this.arr[startpointer]);
+                startpointer++;
+            } else {
+                mergeArray.push(this.arr[midpointer]);
+                midpointer++;
+            }
+            yield;
+        }
+        while(startpointer < mid) {
+            mergeArray.push(this.doRead(startpointer));
+            startpointer++;
+            yield;
+        }
+        while(midpointer < end) {
+            mergeArray.push(this.doRead(midpointer));
+            midpointer++;
+            yield;
+        }
+
+        for(let i = 0; i < mergeArray.length; i++) {
+            this.doWrite(start+i, mergeArray[i]);
+            yield;
+        }
+    }
+}
